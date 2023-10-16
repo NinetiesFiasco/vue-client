acc0831147b2c571825b75b8e1d928a4a0d194d1
Anton Seredin <seredin.anton2014@gmail.com>
2023-10-16 18:36:06 +0300

    Init my vue client

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <HelloWorld />
+  <Tab />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Tab from './components/Tab.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, Tab
+  }
+}
+</script>
+
+<style>
+</style>
